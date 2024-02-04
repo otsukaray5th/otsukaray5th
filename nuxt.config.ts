f2a68555b2ce2e6cc2e8332a73088c91931a7db1
星月なるこ <34113239+naruko-hstk@users.noreply.github.com>
2024-02-04 19:31:07 +0900

    Add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,20 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	devServer: { host: '0.0.0.0' },
-	modules: ['@nuxt/ui', '@nuxtjs/eslint-module'],
+	modules: ['@nuxt/ui', '@nuxtjs/eslint-module', '@nuxtjs/i18n'],
 	eslint: { lintOnStart: true, emitWarning: false },
+	i18n: {
+		langDir: 'locales',
+		locales: [
+			{ code: 'en-us', iso: 'en-US', file: 'en-US.json' },
+			{ code: 'ja-jp', iso: 'ja-JP', file: 'ja-JP.json' },
+			{ code: 'zh-tw', iso: 'zh-TW', file: 'zh-TW.json' },
+			{ code: 'zh-cn', iso: 'zh-CN', file: 'zh-CN.json'},
+			{ code: 'en', iso: 'en-US', file: 'en-US.json'},
+			{ code: 'zh', iso: 'zh-CN', file: 'zh-CN.json'},
+		],
+		defaultLocale: 'ja-jp',
+		detectBrowserLanguage: { useCookie: true },
+		strategy: 'no_prefix',
+	},
 })
